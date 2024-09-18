aa5f180c145d4c1d9b8187383b5d0f421bcf2a17
Floria Fang Zhang <floria.fang9@gmail.com>
2024-09-18 01:04:55 -0400

    Minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 function showFirstProject() {
     // Get all projects into an array
-    const projects = document.getElementsByClassName('col s12 m6 l4');
+    const projects = document.getElementsByClassName('project');
 
     // Hide all projects initially
     for (let i = 0; i < projects.length; i++) {
-    projects[i].style.display = 'none';
+        projects[i].style.display = 'none';
     }
 
     // Display only the first project
     if (projects.length > 0) {
-    projects[0].style.display = 'block';
+        projects[0].style.display = 'block';
     }
 }
 
